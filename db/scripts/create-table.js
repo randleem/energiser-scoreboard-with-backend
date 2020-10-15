af028f9bc65d7f3af1dc0e60a87dc520946db0f1
Dan Watson <dan.watson2309@gmail.com>
2020-10-15 13:48:14 +0100

    zoom crash

---
+++ db/scripts/create-table.js
@@ -0,0 +1,35 @@
+const { query } = require("../index");
+
+// run query to make table
+const sqlUsers = `
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    first_name TEXT,
+    second_name TEXT,
+    user_name TEXT
+);`;
+
+async function createUserTable() {
+  const result = await query(sqlUsers);
+  console.log(result);
+}
+
+createUserTable();
+
+const sqlGames = `
+CREATE TABLE games (
+    game_id SERIAL PRIMARY KEY,
+    date DATE,
+    user_id TEXT,
+    wins INTEGER,
+    points INTEGER
+);`;
+
+async function createGameTable() {
+    const result = await query(sqlGames);
+    console.log(result);
+  }
+
+  createGameTable();
+
+  
