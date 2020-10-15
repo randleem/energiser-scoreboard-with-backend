7d615c2a64c5b87272d1d11f2579d631295a1afe
Emma randle <emmarandle@Emmas-iMac.local>
2020-10-15 23:07:52 +0100

    Emma Push PM

--- model/energiser.js
+++ model/energiser.js
@@ -4,13 +4,13 @@ async function addUser(firstName, lastName, userName) {
     const result = await query("INSERT INTO users (first_name, second_name, user_name) VALUES ($1, $2, $3) RETURNING user_id;", [firstName, lastName, userName]);
     console.log(result.rows[0].user_id)
     return result.rows[0].user_id;
-  }
+    };
 
 async function addGame(date, gamePlayed, user_id, wins, points ) {
     const result = await query("INSERT INTO games (date, game_played, user_id, wins, points ) VALUES ($1, $2, $3, $4, $5) RETURNING game_id;", [date, gamePlayed, user_id, wins, points]);
     console.log(result);
     return result.rows[0].game_id;
-    }
+    };
 
 async function getAllWins() {
     const result = await query("SELECT SUM(wins) AS total_wins, first_name FROM users INNER JOIN games ON users.user_id = games.user_id GROUP BY users.first_name;");
@@ -18,11 +18,11 @@ async function getAllWins() {
     return result.rows;
     };
 
-    async function getTop5() {
-        const result = await query("SELECT SUM(wins) AS total_wins, CONCAT(first_name, ' ', second_name) AS name FROM users INNER JOIN games ON users.user_id = games.user_id GROUP BY name ORDER BY total_wins DESC LIMIT 5;");
-        console.log(result.rows);
-        return result.rows;
-        };
+async function getTop5() {
+    const result = await query("SELECT SUM(wins) AS total_wins, CONCAT(first_name, ' ', second_name) AS name FROM users INNER JOIN games ON users.user_id = games.user_id GROUP BY name ORDER BY total_wins DESC LIMIT 5;");
+    console.log(result.rows);
+    return result.rows;
+    };
 
 // getAllWins();
 
---
+++ public/main.js
@@ -0,0 +1,10 @@
+
+
+
+const pictionaryPic = document.querySelector("#pictionary-pic");
+const mindPic = document.querySelector("#mind-pic");
+const scattegoriesPic = document.querySelector("#scattergories-pic");
+
+pictionaryPic.addEventListener("click", getPictionaryTop5);
+mindPic.addEventListener("click", getMindTop5);
+scattegoriesPic.addEventListener("click", getScattegoriesTop5);
