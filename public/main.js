0a0876a5f68d21110a27eb57dcd48622e857f3bd
Emma randle <emmarandle@Emmas-iMac.local>
2020-10-16 08:44:53 +0100

    Morning Tinkers

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+const leaderBoard = document.querySelector("#table-scores");
 
 //Game Pic Selection
 const pictionaryPic = document.querySelector("#pictionary-pic");
@@ -5,22 +6,62 @@ const mindPic = document.querySelector("#mind-pic");
 const scattegoriesPic = document.querySelector("#scattergories-pic");
 
 //Game Event Listeners
-pictionaryPic.addEventListener("click", getPictionaryTop5);
+// pictionaryPic.addEventListener("click", getPictionaryTop5);
+
 // mindPic.addEventListener("click", getMindTop5);
 // scattegoriesPic.addEventListener("click", getScattegoriesTop5);
 
-//Create Functions that get top5 for all game categories
-async function getPictionaryTop5(){
-    console.log("Hello");
-    const response = await fetch("http://localhost:3000/energiser/getPicionaryTop5");
-    const {payload} = await response.json();
-    console.log(payload);
-}
-
-// async function getRecipes() {
-//     const response = await fetch(`${url}/recipes`);
-//     const { payload } = await response.json();
-//     recipesSection.innerHTML = "";
+// Create Functions that get top5 for all game categories
+// async function getPictionaryTop5(){
+//     console.log("Hello");
+//     const response = await fetch("http://localhost:3000/energiser/getPicionaryTop5");
+//     const {payload} = await response.json();
 //     console.log(payload);
-//     payload.forEach(renderRecipe);
-//   }
+// }
+
+pictionaryPic.addEventListener("click", test);
+
+const payload = [
+    { first_name: 'Mell', total_wins: '13', total_points: '23' },
+    { first_name: 'Emma', total_wins: '9', total_points: '10' },
+    { first_name: 'Lizzie', total_wins: '4', total_points: '6' },
+    { first_name: 'Ben', total_wins: '1', total_points: '1' },
+    { first_name: 'Dan', total_wins: '1', total_points: '1' }
+  ];
+
+function test(){
+    mindPic.src="";
+    scattegoriesPic.src="";
+    addLeaderCols();
+    payload.forEach(addLeaderRows);
+}
+// Creates the Rows for the Table
+function addLeaderCols(){
+    const row = document.createElement("tr");
+    const rowFN = document.createElement("th");
+    const rowTW = document.createElement("th");
+    const rowTP = document.createElement("th");
+    rowFN.innerText = "First Name";
+    rowTW.innerText = "Total Wins";
+    rowTP.innerText = "Total Points";
+    row.appendChild(rowFN);
+    row.appendChild(rowTW);
+    row.appendChild(rowTP);
+    leaderBoard.appendChild(row);
+    leaderBoard.classList.add("col-names");
+};
+
+// Creates the Leaders Rows
+function addLeaderRows(player){
+    const row = document.createElement("tr");
+    const rowFN = document.createElement("th");
+    const rowTW = document.createElement("th");
+    const rowTP = document.createElement("th");
+    rowFN.innerText = player.first_name;
+    rowTW.innerText = player.total_wins;
+    rowTP.innerText = player.total_points;
+    row.appendChild(rowFN);
+    row.appendChild(rowTW);
+    row.appendChild(rowTP);
+    leaderBoard.appendChild(row);
+};
